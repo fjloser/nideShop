ea6998ac724c635671a4ab29a84f65c1ca1b6a92
fangjie <1848052880@qq.com>
2021-05-25 20:59:30 +0800

    taishi

--- pages/kinds/kinds.js
+++ pages/kinds/kinds.js
@@ -6,14 +6,9 @@ Page({
    */
   data: {
     typeId: 1,
-    type: [{
-      typeId: 1,
-      title: '食品酒水'
-    },
-      {
-        typeId: 2,
-        title: '生鲜果蔬'
-      },
+    showData: [],
+    kind: [],
+    type: [
       {
         typeId: 3,
         title: '美妆护肤'
@@ -21,10 +16,6 @@ Page({
       {
         typeId: 4,
         title: '个护清洁'
-      },
-      {
-        typeId: 5,
-        title: '精品男装'
       },
       {
         typeId: 6,
@@ -92,10 +83,66 @@ Page({
       url: '../goodsList/goodsList?typeId=1',
     })
   },
+  /*构建树形结构*/
+  createTree(arr) { 
+    var newTreeArr = [],
+      nullObj = {},
+      children = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].parent_id == 0) {
+        nullObj = { ...arr[i] };
+        let sonArr = this.findSon(arr[i].good_type_id, arr);
+        if (sonArr.length != 0) {
+          nullObj.children = sonArr;
+        }
+        newTreeArr[newTreeArr.length] = nullObj;
+      }
+    }
+    return newTreeArr;
+  },
+  findSon(id, arr) {
+    let sonArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].parent_id == id) {
+        let nullObj = {};
+        nullObj = { ...arr[i] };
+        let sonCacheArr = this.findSon(arr[i].good_type_id, arr);
+        if (sonCacheArr.length != 0) {
+          nullObj.children = sonCacheArr;
+        }
+        sonArr[sonArr.length] = nullObj;
+      }
+    }
+    return sonArr;
+  },
+  getMyKinds(arr){
+    var newArr = []
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i].type_name.substring(0, 2) == 'fj'){
+        newArr.push(arr[i])
+      }
+    }
+    return newArr
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function (options) {
+    wx.request({
+      method: 'POST',
+      url: 'http://api_devs.wanxikeji.cn/api/goodType',
+      success: res => {
+        this.data.kind = this.getMyKinds(res.data.data)
+        this.setData({
+          showData: this.createTree(this.data.kind)
+        })
+        this.setData({
+          typeId: this.data.showData[0].good_type_id
+        })
+        console.log(this.data.showData)
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
